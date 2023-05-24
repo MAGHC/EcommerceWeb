2ff82c5db6d28114d7aa71c887e5f9e5d85446a0
MAGHC <windowsapple95@gmail.com>
2023-05-24 16:11:32 +0900

    feat: Delare Route Path

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Layout from './Layout';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout></Layout>,
+    children: [
+      {
+        index: true,
+        element: <p>home page</p>,
+      },
+      {
+        path: '/admin',
+        element: <p>adimn page</p>,
+      },
+      { path: '/cart', element: <p>cart page</p> },
+      { path: 'product/:id', element: <p>product detail</p> },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
