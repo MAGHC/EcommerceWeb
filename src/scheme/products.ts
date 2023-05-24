94dd976ee0cd799b7d60d4a9ec59dd7396abc076
MAGHC <windowsapple95@gmail.com>
2023-05-24 16:56:11 +0900

    feat : write Product scheme

---
+++ src/scheme/products.ts
@@ -0,0 +1,9 @@
+export type Product = {
+  id: string;
+  description: string;
+  hotsale: boolean;
+  imgs: string[] | string;
+  options: string[] | string;
+  title: string;
+  price: number;
+};
