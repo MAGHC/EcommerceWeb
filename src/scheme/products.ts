5f155a00ef5d869f760945fb625521328a6fbfa1
MAGHC <windowsapple95@gmail.com>
2023-05-24 22:13:18 +0900

    fix : Product imgs scheme fix

--- src/scheme/products.ts
+++ src/scheme/products.ts
@@ -2,7 +2,7 @@ export type Product = {
   id: string;
   description: string;
   hotsale: boolean;
-  imgs: string[] | string;
+  imgs: string[];
   options: string[] | string;
   title: string;
   price: number;
