e21c2d7a2aba662e38b8f85538b4056c7748cb79
MAGHC <windowsapple95@gmail.com>
2023-05-24 21:52:05 +0900

    feat : addProduct

--- src/service/firebase.ts
+++ src/service/firebase.ts
@@ -1,6 +1,13 @@
 import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, get, child } from 'firebase/database';
-import { Product } from '../scheme/products';
+import { getDatabase, ref, get, child, set } from 'firebase/database';
+import { NewProductType, Product } from '../scheme/products';
+import {
+  getDownloadURL,
+  getStorage,
+  ref as stroageRef,
+  uploadBytes,
+  listAll,
+} from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -8,20 +15,51 @@ const firebaseConfig = {
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
 };
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase();
 const dbRef = ref(getDatabase());
+const storage = getStorage();
 
 export async function getAllProducts(): Promise<Product[] | []> {
   return await get(child(dbRef, 'products')).then((snapshot) => {
     if (snapshot.exists()) {
       console.log(snapshot.val());
-      return snapshot.val();
+      return Object.values(snapshot.val());
     } else {
       console.log('no');
       return [];
     }
   });
+}
+
+export async function addProduct(newProduct: NewProductType, imgs: Blob[]) {
+  const id = Date.now().toString();
+
+  const imgListRef = stroageRef(storage, `imgages/${id}`);
+
+  const uploadProcess = imgs.map(async (img, i) => {
+    const imgRef = stroageRef(storage, `imgages/${id}/${i}`);
+    await uploadBytes(imgRef, img);
+  });
+
+  await Promise.all(uploadProcess);
+
+  const imageList = (await listAll(imgListRef)).items;
+
+  const imageUrls: string[] = [];
+
+  const convertUrlProcess = imageList.map(async (item) => {
+    const res = await getDownloadURL(item);
+    imageUrls.push(res);
+  });
+
+  await Promise.all(convertUrlProcess);
+
+  const product = { ...newProduct, id, imgs: imageUrls };
+
+  const res = await set(child(dbRef, `products/${id}`), product);
+  return res;
 }
