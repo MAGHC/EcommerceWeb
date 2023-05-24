096420cdc65db91337fa5aa4cffc59eb256f3290
MAGHC <windowsapple95@gmail.com>
2023-05-24 22:08:16 +0900

    fix: ProductCard logic => pass product State

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,22 +1,25 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Product } from '../../scheme/products';
 import style from './ProductCard.module.css';
 
 const ProductCard = ({ product }: { product: Product }) => {
+  const navigate = useNavigate();
+
   return (
-    <li className={style.container}>
-      <Link to={`/product/${product.id}`}>
-        <div className={style.mainImg}>
-          <img src={product.imgs[0]} alt="productMainImg" />
-        </div>
-        <div>
-          <strong>{product.title}</strong>
-        </div>
-        <div>
-          <strong>{product.price}</strong>
-        </div>
-        <strong> #{product.categorie}</strong>
-      </Link>
+    <li
+      onClick={() => navigate(`product/${product.id}`, { state: { product } })}
+      className={style.container}
+    >
+      <div className={style.mainImg}>
+        <img src={product.imgs[0]} alt="productMainImg" />
+      </div>
+      <div>
+        <strong>{product.title}</strong>
+      </div>
+      <div>
+        <strong>{product.price}</strong>
+      </div>
+      <strong> #{product.categorie}</strong>
     </li>
   );
 };
